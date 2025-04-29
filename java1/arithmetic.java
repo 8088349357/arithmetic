f7b791cd607969ca8169561bb34ed252087c7cfe
8088349357 <santu78634@gmail.com>
2025-04-29 10:26:30 +0530

    Update and rename employee.java to arithmetic.java

---
+++ java1/arithmetic.java
@@ -0,0 +1,12 @@
+
+
+public class arithmetic {
+    public static void main(String[] args) {
+        int a = 10 ;
+        int b = 20;
+        int sum =a+b;
+        int mul=a*b;
+        System.out.println("sum:"+sum);
+        System.out.println("multi:"+mul);    }
+
+}
