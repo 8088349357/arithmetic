c741301fed0cfcf460b0ad18fe07563f6394e279
8088349357 <santu78634@gmail.com>
2025-04-29 10:29:36 +0530

    saved

--- java1/arithmetic.java
+++ java1/arithmetic.java
@@ -1,10 +1,15 @@
 public class arithmetic {
     public static void main(String[] args) {
-        int a = 10 ;
+        int a = 10;
         int b = 20;
-        int sum =a+b;
-        int mul=a*b;
-        System.out.println("sum:"+sum);
-        System.out.println("multi:"+mul);    }
+        int sum = a + b;
+        int mul = a * b;
+        double sqrtA = Math.sqrt(a);
+        double sqrtB = Math.sqrt(b);
 
+        System.out.println("Sum: " + sum);
+        System.out.println("Multiplication: " + mul);
+        System.out.println("Square root of " + a + ": " + sqrtA);
+        System.out.println("Square root of " + b + ": " + sqrtB);
+    }
 }
